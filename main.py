8b652c3a692daa3c660ed853891338ea6e82a3eb
Davi Ramalho <davivargasdev@gmail.com>
2026-03-06 16:50:19 -0300

    Adição de novas normalizações e executavel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from tkinter import Tk, filedialog
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill, Font
 
+#Função para deixar o usuário escolher as planilhas
 def escolher_planilha(titulo):
     root= Tk()
     root.withdraw() 
@@ -11,28 +14,88 @@ def escolher_planilha(titulo):
     if not caminho:
         raise Exception("Nenhum arquivo selecionado")
     return caminho
-#associa as planilhas a "variaveis" internas
-planilha1 = escolher_planilha("Selecione a PRIMEIRA planilha (dados principais)")
-planilha2 = escolher_planilha("Selecione a SEGUNDA planilha (linhas)")
-#faz a junção das 2 planilhas, sendo que utiliza a coluna ID para tal, criando uma variavel temporaria
-planilhaTemp = pd.merge(planilha1, planilha2, on='ID')
 
-#Define user como a coluna Usuario, assim puxando todos os valores dessa coluna
-user=planilhaTemp['Usuario']
-status=planilhaTemp['status']
+def escolher_onde_salvar(titulo, nome):
+    root= Tk()
+    root.withdraw()
+    caminho = filedialog.asksaveasfilename(
+        title=titulo,
+        defaultextension=".xlsx",
+        initialfile="nome",
+        filetypes=[("Arquivos Excel",'*.xlsx')]
+    )
+    if not caminho:
+        raise Exception("Local para salvar não selecionado")
+    return caminho
+
+#Função criada para fins de organização(manda a coluna 'Valor' para ser a ultima )
+def mover_para_final(df, coluna):
+    cols = [c for c in df.columns if c != coluna] + [coluna]
+    return df[cols]
+
+#Usuário escolhe as planilhas que serão utilizadas
+caminho_planilha1 = escolher_planilha("Selecione a PRIMEIRA planilha (dados principais)")
+caminho_planilha2 = escolher_planilha("Selecione a SEGUNDA planilha (linhas)")
+
+#associa as planilhas a "variaveis" internas
+planilha1 = pd.read_excel(caminho_planilha1)
+planilha2 = pd.read_excel(caminho_planilha2, sheet_name="Lista com Valores")
+planilha2 = planilha2[['MSISDN', 'Status', 'Total Linha']]
+
+#Altera o nome de 2 colunas para fins de normalização
+planilha2 = planilha2.rename(columns={
+    'Total Linha': 'Valor'
+})
+planilha1 = planilha1.rename(columns={
+    'Setor_CNPJ': 'Setor'
+})
+
+#Transforma as colunas em minuscula para fins de normalização de dados
+planilha1.columns = planilha1.columns.str.strip().str.lower()
+planilha2.columns = planilha2.columns.str.strip().str.lower()
+
+#Transforma as colunas msisdn em str para não dar conflito
+planilha1['msisdn'] = planilha1['msisdn'].astype(str)
+planilha2['msisdn'] = planilha2['msisdn'].astype(str)
+
+#Retira o "55" presente em alguns numeros
+planilha1['msisdn'] = planilha1['msisdn'].str.replace(r'^55', '', regex=True)
+planilha2['msisdn'] = planilha2['msisdn'].str.replace(r'^55', '', regex=True)
+
+planilha2['valor'] = (
+    planilha2['valor']
+    .astype(str)
+    .str.replace('.', '', regex=False)   # remove separador de milhar
+    .str.replace(',', '.', regex=False)  # troca vírgula por ponto
+)
+
+#Transforma os valores da coluna "valor" de str para int
+planilha2['valor'] = pd.to_numeric(planilha2['valor'], errors='coerce')
+#faz a junção das 2 planilhas, sendo que utiliza a coluna ID para tal, criando uma variavel temporaria
+planilhaTemp = pd.merge(planilha1, planilha2, on='msisdn', how='left')
+
+#Define user como a coluna Usuario que será utilizada para puxar todos os valores dessa coluna
+user=planilhaTemp['usuario']
+status=planilhaTemp['status_atual']
+linha=planilhaTemp['msisdn']
+
+msisdn_vazio= linha.isna()| (linha.astype(str).str.strip()=='')
 #Filtro para caso uma linha não possua dono, ou seja, está livre
 filtro_livre=(
     user.isna()|
-    user.astype(str).str.strip().str.lower().isin(['','não encontrado'])|
     status.astype(str).str.strip().str.lower().isin(['demitido'])
 )
 
 #Filtro para caso uma linha possua dono, ou seja, está ocupado
 filtro_ocupado=(
+    (
     user.notna()&
     ~user.astype(str).str.strip().str.lower().isin(['','não encontrado'])&
     ~status.astype(str).str.strip().str.lower().isin(['demitido'])
+    |msisdn_vazio
+    )
 )
+
 
 #criação da planilha de linhas livres no sistema utilizando o filtro livre criado anteriormente 
 linhas_livres= planilhaTemp[filtro_livre]
@@ -40,33 +103,75 @@ linhas_livres= planilhaTemp[filtro_livre]
 #criação da planilha planilhaFinal no sistema utilizando o filtro ocupado criado anteriormente 
 planilhaFinal=planilhaTemp[filtro_ocupado]
 
-#Função criada para fins de organização(manda a coluna 'Valor' para ser a ultima )
-def mover_para_final(df, coluna):
-    cols = [c for c in df.columns if c != coluna] + [coluna]
-    return df[cols]
-
-planilhaFinal = mover_para_final(planilhaFinal, 'Valor')
-linhas_livres = mover_para_final(linhas_livres, 'Valor')
+planilhaFinal = mover_para_final(planilhaFinal, 'valor')
+linhas_livres = mover_para_final(linhas_livres, 'valor')
 
 #agrupamento dos valores baseado nos setores(quanto cada setor gasta com as linhas)
-valor_total=planilhaTemp.groupby('Setor')['Valor'].sum()
-valor_livre=linhas_livres.groupby('Setor')['Valor'].sum()
-valor_ocupado=planilhaFinal.groupby('Setor')['Valor'].sum()
+valor_total=planilhaTemp.groupby('setor')['valor'].sum()
+valor_livre=linhas_livres.groupby('setor')['valor'].sum()
+valor_ocupado=planilhaFinal.groupby('setor')['valor'].sum()
 
+#criação das colunas presentes na segunda aba da planilha final
 valor_setor=pd.DataFrame({
-    'Setor': valor_total.index,
-    'Valor Linhas Livres': valor_livre.reindex(valor_total.index, fill_value=0).values,
-    'Valor Linhas Ocupadas': valor_ocupado.reindex(valor_total.index, fill_value=0).values,
-    'Valor Total': valor_total.values,
+    'setor': valor_total.index,
+    'valor linhas livres': valor_livre.reindex(valor_total.index, fill_value=0).values,
+    'valor linhas ocupadas': valor_ocupado.reindex(valor_total.index, fill_value=0).values,
+    'valor total': valor_total.values,
     })
 #Print no sistema para motivos de teste rápido
 print(planilhaFinal)
 
-
-#criação da planilha definitiva contendo os valores pagos mensalmente
-with pd.ExcelWriter('C:/Users/davi.ramalho/Desktop/Estudos python/planilha_com_abas.xlsx') as writer:
+#chama a função de salvar dados e envia os parametros
+caminho_excel=escolher_onde_salvar(
+    "Salvar relatório",
+    "relatório_final.xlsx"
+)
+#cria a planilha final com as 2 abas
+base= caminho_excel.rsplit('.', 1)[0]
+with pd.ExcelWriter(base+".xlsx") as writer:
     planilhaFinal.to_excel(writer, sheet_name='Usuários Válidos', index=False)
-    valor_setor.to_excel(writer, sheet_name='Valor-Setor',index=False)
-#criação das planilhas de saída nos arquivos do computador, pronto para serem abertas.
-planilhaFinal.to_excel('C:/Users/davi.ramalho/Desktop/Estudos python/planilha_final.xlsx', index=False)
-linhas_livres.to_excel('C:/Users/davi.ramalho/Desktop/Estudos python/linhas_livres.xlsx', index=False)
+    valor_setor.to_excel(writer, sheet_name='Valor-Setor', index=False)
+
+wb= load_workbook(base+".xlsx")
+ws= wb['Usuários Válidos']
+
+#Estilo cabeçalho
+ws.freeze_panes ="A2" #congela cabeçalho
+header_fill= PatternFill(start_color="33FF33", end_color="33FF33", fill_type="solid")
+
+for cell in ws[1]:
+    cell.font= Font(bold=True, color="FFFFFF")
+    cell.fill= header_fill
+
+#Ajusta a largura das colunas
+for col in ws.columns:
+    max_length= 0
+    col_letter= col[0].column_letter
+
+    for cell in col:
+        try:
+            if cell.value:
+                max_length=max(max_length, len(str(cell.value)))
+        except:
+            pass
+    ws.column_dimensions[col_letter].width= max_length+2
+
+ws.auto_filter.ref = ws.dimensions
+
+wb.save(base+'.xlsx')
+
+#cria o arquivo .csv da planilha final
+planilhaFinal.to_csv(
+    base+".csv",
+    index=False,
+    sep=";",
+    encoding='utf-8-sig'
+)
+
+#cria o arquivo .csv da planilha linhas livres
+linhas_livres.to_csv(
+    base+"linhas_livres.csv",
+    index=False,
+    sep=";",
+    encoding='utf-8-sig'
+)
